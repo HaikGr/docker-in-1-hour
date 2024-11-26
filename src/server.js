d4fd3856c4497e6710af8455da082b555cd9f9f0
Haik Grigoryan <haykgrigoryanpersonal@gmail.com>
2024-11-26 19:21:25 +0400

    m

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-     res.send("Dev branch action that create DockerImage and deploy to my server on port 4321! Updated"); 
+     res.send("Dev branch action that create DockerImage and deploy to my server on port 4321! Update"); 
  });
 
 app.listen(3000, function () {
